dfac10a98618b69443be67f5ba73c5c5efb4b626
yooyoo <lethucntt1@gmail.com>
2018-01-17 10:04:14 +0700

    update migrate

---
+++ database/migrations/2018_01_17_024345_create_members_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('information');
+            $table->string('phone_number');
+            $table->date('birthday');
+            $table->string('avatar');
+            $table->integer('position_id');
+            $table->string('position');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
---
+++ database/migrations/2018_01_17_024500_create_member_projects_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMemberProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('member_projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('meber_id');
+            $table->integer('project_id');
+            $table->string('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('member_projects');
+    }
+}
